73919dfb9f5bfd12cae51751769e17f4b199aabe
guishijin <420188751@qq.com>
2018-08-22 16:39:05 +0800

    CSS Moudle

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,8 @@ module.exports = {
                         options: {
                             modules: true
                         }
+                    },{
+                        loader: "postcss-loader"
                     }
                 ]
             }
