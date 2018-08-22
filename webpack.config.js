7977e8d2dbb22075152eed4e643bf2186b29ef3b
guishijin <420188751@qq.com>
2018-08-22 16:31:13 +0800

    CSS Moudle

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,10 @@ module.exports = {
                     {
                         loader: "style-loader"
                     }, {
-                        loader: "css-loader"
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
                     }
                 ]
             }
